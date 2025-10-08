c287a11fc1d9a17e88dbfa258b1074431888add0
Sindou <sindouatara@hotmail.fr>
2025-10-08 11:50:43 +0000

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home page</Text>
+        </View>
+    );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+
+});
